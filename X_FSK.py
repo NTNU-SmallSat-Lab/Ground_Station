88379a06ce03588e8b6f80490b867e96f3289844
ErikBuer <47660770+ErikBuer@users.noreply.github.com>
2019-02-16 15:14:16 +0100

    Added a receiver (not working)

--- X_FSK.py
+++ X_FSK.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: X_FSK
-# Generated: Fri Feb 15 16:57:00 2019
+# Generated: Sat Feb 16 15:11:26 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -19,6 +19,7 @@ if __name__ == '__main__':
 from PyQt4 import Qt
 from gnuradio import analog
 from gnuradio import blocks
+from gnuradio import channels
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
@@ -62,32 +63,33 @@ class X_FSK(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 48e3
+        self.samp_rate = samp_rate = 480007
+        self.IO_FLT_Cutoff = IO_FLT_Cutoff = 100e3
 
         ##################################################
         # Blocks
         ##################################################
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
         	1024, #size
         	samp_rate, #samp_rate
         	"", #name
         	1 #number of inputs
         )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0_0.set_y_axis(-1, 1)
 
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+        self.qtgui_time_sink_x_0_0.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_0.enable_tags(-1, True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+        self.qtgui_time_sink_x_0_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0.enable_grid(False)
+        self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0_0.enable_stem_plot(False)
 
         if not True:
-          self.qtgui_time_sink_x_0.disable_legend()
+          self.qtgui_time_sink_x_0_0.disable_legend()
 
         labels = ['', '', '', '', '',
                   '', '', '', '', '']
@@ -104,33 +106,71 @@ class X_FSK(gr.top_block, Qt.QWidget):
 
         for i in xrange(1):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
+        self.qtgui_sink_x_0_0 = qtgui.sink_c(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"", #name
+        	True, #plotfreq
+        	True, #plotwaterfall
+        	True, #plottime
+        	True, #plotconst
+        )
+        self.qtgui_sink_x_0_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_0_win = sip.wrapinstance(self.qtgui_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_0_win)
+
+        self.qtgui_sink_x_0_0.enable_rf_freq(False)
+
+
+
+        self.low_pass_filter_0_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, IO_FLT_Cutoff, 10, firdes.WIN_HAMMING, 6.76))
         self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
-        	1, samp_rate, 20e3, 10, firdes.WIN_KAISER, 6.76))
+        	1, samp_rate, IO_FLT_Cutoff, 10, firdes.WIN_HAMMING, 6.76))
+        self.channels_channel_model_0 = channels.channel_model(
+        	noise_voltage=0.01,
+        	frequency_offset=0.0,
+        	epsilon=1.0,
+        	taps=(1.0 + 1.0j, ),
+        	noise_seed=0,
+        	block_tags=False
+        )
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
         self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_multiply_const_vxx_0_1_0 = blocks.multiply_const_vff((-1, ))
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((-1, ))
+        self.blocks_moving_average_xx_0 = blocks.moving_average_ff(10, 0.01, 4000, 1)
         self.blocks_float_to_complex_0_0_0 = blocks.float_to_complex(1)
         self.blocks_float_to_complex_0_0 = blocks.float_to_complex(1)
         self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_file_sink_0_1_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'C:\\Users\\buer9\\Google Drive\\ELSYS\\Student_defined_project\\Gnu_radio\\Ground_Station\\file_sink\\receive_complex.bin', False)
+        self.blocks_file_sink_0_1_0.set_unbuffered(False)
+        self.blocks_file_sink_0_1 = blocks.file_sink(gr.sizeof_float*1, 'C:\\Users\\buer9\\Google Drive\\ELSYS\\Student_defined_project\\Gnu_radio\\Ground_Station\\file_sink\\receive_float.bin', False)
+        self.blocks_file_sink_0_1.set_unbuffered(False)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_float*1, 'C:\\Users\\buer9\\Google Drive\\ELSYS\\Student_defined_project\\Gnu_radio\\Ground_Station\\file_sink\\complex_binary_feed.bin', False)
+        self.blocks_file_sink_0_0.set_unbuffered(False)
+        self.blocks_delay_1 = blocks.delay(gr.sizeof_float*1, 1000)
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.blocks_add_const_vxx_0 = blocks.add_const_vff((1, ))
-        self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate, analog.GR_SQR_WAVE, 9600, 1, 0)
+        self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate, analog.GR_SQR_WAVE, 4.8e3, 1, 0)
         self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1500, 1, 0)
-        self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(samp_rate/(2*math.pi*1e3/8.0))
+        self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf(samp_rate/(2*math.pi*3e3/8.0))
+        self.analog_pwr_squelch_xx_0 = analog.pwr_squelch_cc(-40, 1e-4, 0, True)
         self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0)
 
 
@@ -140,24 +180,33 @@ class X_FSK(gr.top_block, Qt.QWidget):
         ##################################################
         self.connect((self.analog_const_source_x_0, 0), (self.blocks_float_to_complex_0_0, 1))
         self.connect((self.analog_const_source_x_0, 0), (self.blocks_float_to_complex_0_0_0, 1))
-        self.connect((self.analog_quadrature_demod_cf_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.analog_pwr_squelch_xx_0, 0), (self.analog_quadrature_demod_cf_0, 0))
+        self.connect((self.analog_pwr_squelch_xx_0, 0), (self.blocks_file_sink_0_1_0, 0))
+        self.connect((self.analog_quadrature_demod_cf_0, 0), (self.blocks_moving_average_xx_0, 0))
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_complex_to_float_0, 0))
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_float_to_complex_0_0, 0))
-        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_delay_1, 0))
         self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_float_to_complex_0_0_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_float_to_complex_0, 0))
         self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_multiply_const_vxx_0_1_0, 0))
+        self.connect((self.blocks_delay_1, 0), (self.blocks_file_sink_0_0, 0))
+        self.connect((self.blocks_delay_1, 0), (self.blocks_float_to_complex_0_0, 0))
+        self.connect((self.blocks_delay_1, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_xx_0_0, 1))
         self.connect((self.blocks_float_to_complex_0_0, 0), (self.blocks_multiply_xx_0, 1))
         self.connect((self.blocks_float_to_complex_0_0_0, 0), (self.blocks_multiply_xx_0_0, 0))
+        self.connect((self.blocks_moving_average_xx_0, 0), (self.blocks_file_sink_0_1, 0))
+        self.connect((self.blocks_moving_average_xx_0, 0), (self.qtgui_time_sink_x_0_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0_1_0, 0), (self.blocks_float_to_complex_0, 1))
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_add_xx_0, 1))
-        self.connect((self.blocks_throttle_0, 0), (self.analog_quadrature_demod_cf_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.channels_channel_model_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.low_pass_filter_0_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.qtgui_sink_x_0_0, 0))
+        self.connect((self.low_pass_filter_0_0, 0), (self.analog_pwr_squelch_xx_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "X_FSK")
@@ -169,12 +218,22 @@ class X_FSK(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 20e3, 10, firdes.WIN_KAISER, 6.76))
+        self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate)
+        self.qtgui_sink_x_0_0.set_frequency_range(0, self.samp_rate)
+        self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, self.IO_FLT_Cutoff, 10, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.IO_FLT_Cutoff, 10, firdes.WIN_HAMMING, 6.76))
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.analog_quadrature_demod_cf_0.set_gain(self.samp_rate/(2*math.pi*1e3/8.0))
+        self.analog_quadrature_demod_cf_0.set_gain(self.samp_rate/(2*math.pi*3e3/8.0))
+
+    def get_IO_FLT_Cutoff(self):
+        return self.IO_FLT_Cutoff
+
+    def set_IO_FLT_Cutoff(self, IO_FLT_Cutoff):
+        self.IO_FLT_Cutoff = IO_FLT_Cutoff
+        self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, self.IO_FLT_Cutoff, 10, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.IO_FLT_Cutoff, 10, firdes.WIN_HAMMING, 6.76))
 
 
 def main(top_block_cls=X_FSK, options=None):
--- file_sink/XFSK_ANALYZER.m
+++ file_sink/XFSK_ANALYZER.m
@@ -2,7 +2,7 @@ clear all
 clc
 
 
-FID = fopen("output.bin"); % import IQ from TX block
+FID = fopen("receive_complex.bin"); % import IQ from TX block
 if FID == -1, error('Cannot open file'); end
 Datastring=fread(FID,'single');   % By default, fread reads a file 1 byte at a time, interprets each byte as an 8-bit unsigned integer (uint8), and returns a double array.
 fclose(FID);
@@ -18,22 +18,38 @@ if FID == -1, error('Cannot open file'); end
 Datastring=fread(FID,'single');   % By default, fread reads a file 1 byte at a time, interprets each byte as an 8-bit unsigned integer (uint8), and returns a double array.
 fclose(FID);
 
-data=Datastring;
+TX_data=Datastring;
+
+FID = fopen("receive_float.bin"); % import byte stream from TX block
+if FID == -1, error('Cannot open file'); end
+Datastring=fread(FID,'single');   % By default, fread reads a file 1 byte at a time, interprets each byte as an 8-bit unsigned integer (uint8), and returns a double array.
+fclose(FID);
+
+RX_data=Datastring;
 
 %for i=2:2:size(Datastring)-1
 %    p=i/2;
 %    data_IQ(p) = complex(Datastring(i),Datastring(i+1));
 %end
 
-figure(1)
+%figure(1)
 %subplot(2,1,1)
-plot(abs(IQ))
+%plot(abs(IQ))
 %plot(real(IQ))
-hold
+%hold
 %plot(imag(IQ))
-grid
+%grid
 
 %subplot(2,1,2)
-plot(data)%real(data_IQ)
+%plot(real(data_IQ)%
 %grid
+
+figure(2)
+plot(RX_data(1000:1999))
+hold
+plot(TX_data(1000:1999)) % (1000:1100)
+%plot(real(IQ(1000:1999))) % (1000:1100)
+%plot(imag(IQ(1000:1999)))
+legend('RX data','TX data','RX real','RX imag')
+grid
 
